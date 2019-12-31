acf4255342fbc8d7ced6fa4cf35471fbd558c3ef
Josh <josh@nexxt.in>
2019-12-31 13:48:35 -0500

    add type hints to `PAMFile`

--- PAM.py
+++ PAM.py
@@ -1,8 +1,10 @@
 from scipy.io import wavfile
 from datetime import datetime
+from pathlib import PosixPath
+from datetime import date
 
 class PAMFile:
-    def __init__(self, path, unit, dt):
+    def __init__(self, path: PosixPath, unit: str, dt: date):
         self.path = path
         self.unit = unit
         self.dt = dt
